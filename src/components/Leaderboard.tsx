a659c9f1cc394242995673487666d7454fe8e8fd
Eric Li <masternode@163.com>
2023-03-25 08:32:07 -0400

    Fix leaderboard hydration by adding tbody tag

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,22 +1,24 @@
 export default function LeaderBoard({ data }: { data: string }) {
+	var leaderboard = JSON.parse(data);
 
-  var leaderboard = JSON.parse(data);
-
+	console.log(leaderboard);
 
 	return (
 		<table>
-			{leaderboard.map((user: any) => (
-				<tr key={user._id}>
-					<td>{user.name}</td>
-					<td>{user.completed}</td>
-					<td>{user.today ? "✔" : "✖"}</td>
-					<td>
-						{new Date(user.timeToday * 1000)
-							.toISOString()
-							.substring(14, 19)}{" "}
-					</td>
-				</tr>
-			))}
+			<tbody>
+				{leaderboard.map((user: any) => (
+					<tr key={user._id}>
+						<td>{user.name}</td>
+						<td>{user.completed}</td>
+						<td>{user.today ? '✔' : '✖'}</td>
+						<td>
+							{new Date(user.timeToday * 1000)
+								.toISOString()
+								.substring(14, 19)}{' '}
+						</td>
+					</tr>
+				))}
+			</tbody>
 		</table>
 	);
 }
