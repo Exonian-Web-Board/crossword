78bebd8dbaa17310898e1ff0dcedd5e557f44b72
Eric Li <ericlmtn@gmail.com>
2023-10-27 21:30:17 -0400

    Minimal working example

--- src/components/wordle/TextBox.tsx
+++ src/components/wordle/TextBox.tsx
@@ -3,17 +3,28 @@ import React from 'react';
 const TextBox = ({
 	letter,
 	answerLetter,
+	answer,
 }: {
 	letter: string;
 	answerLetter: string;
+	answer: string;
 }) => {
 	return (
 		<>
-			{letter == answerLetter.toUpperCase() ? (
+			{letter == answerLetter.toUpperCase() && (
 				<div className="border-2 mx-2 flex items-center justify-center h-12 w-12 text-10 bg-green-500">
 					{letter}
 				</div>
-			) : (
+			)}
+
+			{letter != answerLetter.toUpperCase() &&
+				answer.toUpperCase().includes(letter) && (
+					<div className="border-2 mx-2 flex items-center justify-center h-12 w-12 text-10 bg-yellow-500">
+						{letter}
+					</div>
+				)}
+
+			{!answer.toUpperCase().includes(letter) && (
 				<div className="border-2 mx-2 flex items-center justify-center h-12 w-12 text-10">
 					{letter}
 				</div>
--- src/components/wordle/TextRow.tsx
+++ src/components/wordle/TextRow.tsx
@@ -10,11 +10,31 @@ export default function TextRow({
 }) {
 	return (
 		<div className="flex my-2">
-			<TextBox letter={word[0]} answerLetter={answer[0]} />
-			<TextBox letter={word[1]} answerLetter={answer[1]} />
-			<TextBox letter={word[2]} answerLetter={answer[2]} />
-			<TextBox letter={word[3]} answerLetter={answer[3]} />
-			<TextBox letter={word[4]} answerLetter={answer[4]} />
+			<TextBox
+				letter={word[0]}
+				answerLetter={answer[0]}
+				answer={answer}
+			/>
+			<TextBox
+				letter={word[1]}
+				answerLetter={answer[1]}
+				answer={answer}
+			/>
+			<TextBox
+				letter={word[2]}
+				answerLetter={answer[2]}
+				answer={answer}
+			/>
+			<TextBox
+				letter={word[3]}
+				answerLetter={answer[3]}
+				answer={answer}
+			/>
+			<TextBox
+				letter={word[4]}
+				answerLetter={answer[4]}
+				answer={answer}
+			/>
 		</div>
 	);
 }
